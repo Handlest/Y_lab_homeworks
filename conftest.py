09219dfc5d1381194aee2467b6cee8590ff2c1e3
Maxim <therikomero@gmail.com>
2024-01-31 18:31:49 +0700

    fixes for fixtures and docker compose

--- conftest.py
+++ conftest.py
@@ -1,8 +1,10 @@
 import pytest
+from sqlalchemy.orm import Session
 from starlette.testclient import TestClient
 
 from config import Base, engine, get_test_db
 from main import app
+from models.models import Menu, Submenu, Dish
 from utils import create_menu_json, create_submenu_json, create_dish_json
 
 
@@ -25,20 +27,42 @@ def db():
 
 
 @pytest.fixture
-def create_menu(client: TestClient):
-    response = client.post(url="http://localhost:8000/api/v1/menus", json=create_menu_json())
-    return response.json()['id']
+def create_menu(client: TestClient, db: Session):
+    menu = create_menu_json()
+    db.add(Menu(**menu))
+    db.commit()
+    menu_id = db.query(Menu).filter(Menu.title == menu['title']).first().id
+    return menu_id
+
+@pytest.fixture
+def create_submenu(create_menu, client: TestClient, db: Session):
+    menu = create_menu_json()
+    db.add(Menu(**menu))
+    db.commit()
+    menu_id = db.query(Menu).filter(Menu.title == menu['title']).first().id
+    submenu = create_submenu_json()
+    submenu['menu_id'] = menu_id
+    db.add(Submenu(**submenu))
+    db.commit()
+    submenu_id = db.query(Submenu).filter(Submenu.title == submenu['title']).first().id
+    return menu_id, submenu_id
 
 
 @pytest.fixture
-def create_submenu(create_menu, client: TestClient):
-    response = client.post(url=f"http://localhost:8000/api/v1/menus/{create_menu}/submenus", json=create_submenu_json())
-    return create_menu, response.json()['id']
+def create_dish(create_submenu, client: TestClient, db: Session):
+    menu = create_menu_json()
+    db.add(Menu(**menu))
+    db.commit()
+    menu_id = db.query(Menu).filter(Menu.title == menu['title']).first().id
+    submenu = create_submenu_json()
+    submenu['menu_id'] = menu_id
+    db.add(Submenu(**submenu))
+    db.commit()
+    submenu_id = db.query(Submenu).filter(Submenu.title == submenu['title']).first().id
+    dish = create_dish_json()
+    dish['submenu_id'] = submenu_id
+    db.add(Dish(**dish))
+    db.commit()
+    dish_id = db.query(Dish).filter(Dish.title == dish['title']).first().id
+    return menu_id, submenu_id, dish_id
 
-
-@pytest.fixture
-def create_dish(create_submenu, client: TestClient):
-    menu_id = create_submenu[0]
-    submenu_id = create_submenu[1]
-    response = client.post(url=f"http://localhost:8000/api/v1/menus/{menu_id}/submenus/{submenu_id}", json=create_dish_json())
-    return menu_id, submenu_id, response.json()['id']
